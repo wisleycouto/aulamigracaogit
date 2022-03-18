05349151dd374b15653a0397b92737bf9326acb1
Wisley Couto <48127043+wisleycouto@users.noreply.github.com>
2022-03-18 16:03:11 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src='./logo512.png' className="App-logo" alt="logo" />
         <a>
-          Learn React
+         Bem vindo a tela inicial Preza!
         </a>
 
       </header>
