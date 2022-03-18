5330cb5de23311ed41080720f99ad297011d6d39
lucaoliveira <lucaoliveira@mec.gov.br>
2022-03-18 15:18:34 -0300

    ADD

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='./logo512.png' className="App-logo" alt="logo" />
+        <a>
+          Learn React
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
